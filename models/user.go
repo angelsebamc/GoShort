1782bee73bb5128ffb1a88851bee7f8350fee526
angelsebamc <asmcardozoo@gmail.com>
2024-04-21 11:30:55 -0300

    fix created typo

--- models/user.go
+++ models/user.go
@@ -7,5 +7,5 @@ type User struct {
 	Username string    `json:"username" bson:"username,omitempty"`
 	Password string    `json:"password" bson:"password,omitempty"`
 	Email    string    `json:"email" bson:"email,omitempty"`
-	Created  time.Time `json:"created" bson:"created,omitempty" default: time.Now()`
+	Created  time.Time `json:"created" bson:"created,omitempty" default:"time.Now()"`
 }
