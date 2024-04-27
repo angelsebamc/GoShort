15e2bfe8ed9aa606f9516a6d532c3ba58c7dd1f7
angelsebamc <asmcardozoo@gmail.com>
2024-04-27 20:30:43 -0300

    update user dtos

--- dtos/user_dto/user_dto.go
+++ dtos/user_dto/user_dto.go
@@ -3,12 +3,14 @@ package user_dto
 import "time"
 
 type UserDTO_Info struct {
+	ID       string    `json:"id" bson:"_id"`
 	Username string    `json:"username" bson:"username"`
 	Email    string    `json:"email" bson:"username"`
 	Created  time.Time `json:"created" bson:"created"`
 }
 
 type UserDTO_Info_Token struct {
+	ID       string `json:"id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Token    string `json:"token"`
