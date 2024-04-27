e9e8f74581de7603089fe012acf0f95c663ed256
angelsebamc <asmcardozoo@gmail.com>
2024-04-27 17:55:56 -0300

    adding session handlers to main

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"goshort/handlers/session_handler"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	r := gin.Default()
+
 	//Loading environment variables
 	errEnv := godotenv.Load()
 
@@ -14,4 +18,9 @@ func main() {
 		fmt.Println("Error loading .env")
 	}
 
+	//user hanlders
+	r.POST("/api/user/register", session_handler.Register)
+	r.POST("/api/user/login", session_handler.Login)
+
+	r.Run(":8080")
 }
