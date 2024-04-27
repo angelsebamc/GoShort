f7394c50909d63f41d4b164c1932170b05ba1eb3
angelsebamc <asmcardozoo@gmail.com>
2024-04-27 17:53:40 -0300

    add dto for token

--- dtos/user_dto/user_dto.go
+++ dtos/user_dto/user_dto.go
@@ -1,4 +1,4 @@
-package user_dtos
+package user_dto
 
 import "time"
 
@@ -6,6 +6,12 @@ type UserDTO_Info struct {
 	Username string    `json:"username" bson:"username"`
 	Email    string    `json:"email" bson:"username"`
 	Created  time.Time `json:"created" bson:"created"`
+}
+
+type UserDTO_Info_Token struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
 }
 
 type UserDTO_Registration struct {
