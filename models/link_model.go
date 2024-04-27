e055de3dddd7fe425c546f71a068cbde13e312fd
angelsebamc <asmcardozoo@gmail.com>
2024-04-27 20:31:10 -0300

    create link data structs

--- models/link_model.go
+++ models/link_model.go
@@ -1,9 +1,11 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Link struct {
-	ID          string `json:"id" bson:"_id,omitempty"`
-	ShortUrl    string `json:"short_url" bson:"short_url,omitempty"`
-	OriginalUrl string `json:"original_url" bson:"original_url,omitempty"`
-	UserID      string `json:"user_id" bson:"user_id,omitempty"`
-	Clicks      int    `json:"clicks" bson:"clicks,omitempty"`
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	ShortUrl    string             `json:"short_url" bson:"short_url,omitempty"`
+	OriginalUrl string             `json:"original_url" bson:"original_url,omitempty"`
+	UserID      string             `json:"user_id" bson:"user_id,omitempty"`
+	Clicks      int                `json:"clicks" bson:"clicks,omitempty"`
 }
