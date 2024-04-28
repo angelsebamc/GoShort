a11fb434811ffddce39525702eb40e30da8aaaa0
angelsebamc <asmcardozoo@gmail.com>
2024-04-28 19:03:08 -0300

    get jwt token from auth

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -4,6 +4,7 @@ import (
 	"goshort/repositories/user_repository"
 	"goshort/utils/json_response"
 	"goshort/utils/jwt"
+	"strings"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -20,13 +21,20 @@ func Auth() gin.HandlerFunc {
 			return
 		}
 
-		request_jwt := c.Request.Header.Get("Authorization")
-		if request_jwt == "" {
+		request_bearer_jwt := c.Request.Header.Get("Authorization")
+		if request_bearer_jwt == "" {
 			c.AbortWithStatusJSON(403, json_response.New(403, "invalid token", nil))
 			return
 		}
 
-		jwt_claims, claims_err := jwt.GetInstance().ExtractTokenClaims(request_jwt)
+		request_jwt := strings.Split(request_bearer_jwt, " ")
+
+		if len(request_jwt) != 2 || request_jwt[0] != "Bearer" {
+			c.AbortWithStatusJSON(403, json_response.New(403, "invalid token", nil))
+			return
+		}
+
+		jwt_claims, claims_err := jwt.GetInstance().ExtractTokenClaims(request_jwt[1])
 
 		if claims_err != nil {
 			c.AbortWithStatusJSON(500, json_response.New(500, claims_err.Error(), nil))
@@ -45,12 +53,14 @@ func Auth() gin.HandlerFunc {
 
 		if err != nil {
 			c.AbortWithStatusJSON(403, json_response.New(403, "invalid user", nil))
+			return
 		}
 
 		user_from_db := user_repository.GetInstance().GetUserById(user_object_id)
 
 		if user_from_db == nil {
 			c.AbortWithStatusJSON(403, json_response.New(404, "user does not exists", nil))
+			return
 		}
 
 		c.Set("user_email", email_claim)
