75df7d39bb5f91ab3cd47dd98dcd60b816a59e24
angelsebamc <asmcardozoo@gmail.com>
2024-04-29 22:17:35 -0300

    change link userid datatype

--- models/link_model.go
+++ models/link_model.go
@@ -6,6 +6,6 @@ type Link struct {
 	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	ShortUrl    string             `json:"short_url" bson:"short_url,omitempty"`
 	OriginalUrl string             `json:"original_url" bson:"original_url,omitempty"`
-	UserID      string             `json:"user_id" bson:"user_id,omitempty"`
+	UserID      primitive.ObjectID `json:"user_id" bson:"user_id,omitempty"`
 	Clicks      int                `json:"clicks" bson:"clicks,omitempty"`
 }
