145fe893aa64a3cbd2b15c0cc3c66f94a681d589
angelsebamc <asmcardozoo@gmail.com>
2024-05-01 13:35:22 -0300

    delete session from main

--- main.go
+++ main.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 	"goshort/routes/link_routes.go"
 	"goshort/routes/user_routes"
-	"goshort/utils"
 	"goshort/utils/json_response"
 	"net/http"
 	"os"
 
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -24,10 +21,6 @@ func main() {
 	if errEnv != nil {
 		fmt.Println("Error loading .env")
 	}
-
-	//setup session
-	store := cookie.NewStore([]byte(utils.GenerateRandomString(64)))
-	router.Use(sessions.Sessions("goshort", store))
 
 	//not found
 	router.NoRoute(func(c *gin.Context) {
